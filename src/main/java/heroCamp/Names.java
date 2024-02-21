4fb279ca882efb825e81edb928ed36d7ce5f531e
Anton <toshasuhanov@ya.ru>
2024-02-21 22:34:17 +0300

    create coordinate method

---
+++ src/main/java/heroCamp/Names.java
@@ -0,0 +1,6 @@
+package heroCamp;
+
+public enum Names {
+    Oliver, Jack, Harry, Jacob, Charlie, Thomas, Oscar, William, James , George,
+    Amelia, Olivia, Emily, Ava, Jessica, Isabella, Sophie, Mia, Ruby, Lily
+}
---
+++ src/main/java/heroCamp/Place.java
@@ -0,0 +1,10 @@
+package heroCamp;
+
+public class Place {
+    int x, y;
+
+    public Place(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
